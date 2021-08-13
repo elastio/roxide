12e174f57062749a61d51d48c4db163fa54c53f7
kpovnych <kpovnych@elastio.com>
2021-08-13 13:53:46 +0300

    LOG_SIZE_FOR_FLUSH -> 64MB

--- roxide/src/checkpoint.rs
+++ roxide/src/checkpoint.rs
@@ -76,7 +76,7 @@ impl Checkpoint {
             ffi_try!(ffi::rocksdb_checkpoint_create(
                 checkpoint.as_ptr(),
                 cpath.as_ptr(),
-                20 * 1024 * 1024, // Experiment: 20MB
+                64 * 1024 * 1024, // Experiment: 20MB
             ))?;
         }
 
